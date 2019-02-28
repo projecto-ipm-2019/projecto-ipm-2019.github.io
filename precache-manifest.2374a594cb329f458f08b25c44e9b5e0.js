df38e49bc907180f07b0a2bd470c415f26521b94
AndreasBBS <andreas.bbs@gmail.com>
2019-02-28 17:33:34 +0000

    Updates

---
+++ precache-manifest.2374a594cb329f458f08b25c44e9b5e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "68aa7d9a2cac815df80922861943c259",
+    "url": "/static/media/79718.68aa7d9a.jpg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "f703d7944927e8131ec5",
+    "url": "/static/js/main.f703d794.chunk.js"
+  },
+  {
+    "revision": "81739510e03b5daad6bc",
+    "url": "/static/js/2.81739510.chunk.js"
+  },
+  {
+    "revision": "f703d7944927e8131ec5",
+    "url": "/static/css/main.0e663b7c.chunk.css"
+  },
+  {
+    "revision": "90a4f46567bb1419542847ce5ab5fecb",
+    "url": "/index.html"
+  }
+];
